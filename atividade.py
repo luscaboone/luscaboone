f8e76a57936c4d628d217452c70c43859316f721
Lucas <160123367+luscaboone@users.noreply.github.com>
2024-02-29 10:33:29 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,5 +1,5 @@
 #mostrar alo mundo na tela
-print('Ola Mundo!')
+print('Alo Mundo!')
 #pedir para o usuario digitar um numero e entao imprimir na tela o numero digitado
 numero = int(input('Digite um numero: '))
 print("Seu numero digitado foi: ", numero)
@@ -19,9 +19,10 @@ inteiro = int(50)
 float = 3.75
 print(f'{string} {type(string)} minha classificacao e: string.\n{inteiro} {type(inteiro)} minha classificacao e: int.\n{float} {type(float)} minha classificacao e: float')
 # tabuada
-tabuada = int(input('Digite o numero que voce queira saber a tabuada: '))
-calculo01,calculo02,calculo03,calculo04,calculo05,calculo06,calculo07,calculo08,calculo09,calculo10 = (tabuada)*1,(tabuada)*2,(tabuada)*3,(tabuada)*4,(tabuada)*5,(tabuada)*6,(tabuada)*7,(tabuada)*8,(tabuada)*9,(tabuada)*10
-print(f'A tabuada ate o numero 10 de {tabuada} sera!\n{tabuada}X1={calculo01}\n{tabuada}X2={calculo02}\n{tabuada}X3={calculo03}\n{tabuada}X4={calculo04}\n{tabuada}x5={calculo05}\n{tabuada}X6={calculo06}\n{tabuada}X7={calculo07}\n{tabuada}X8={calculo08}\n{tabuada}x9={calculo09}\n{tabuada}X10={calculo10}')
+n = int(input('Digite um numero para saber a tabuada: '))
+print(f'A tabuada de {n} será: ')
+for i in range (11): 
+    print(f'{n}X{i}={n*i}')
 #calculadora basica
 primeironumero = int(input('digite um numero: '))
 segundonumero = int(input('digite outro numero: '))
